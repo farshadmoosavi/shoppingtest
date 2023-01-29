fb12babb8b762bb13c3ebf75735049fc4f19c28f
farshadmoosavi <97333681+farshadmoosavi@users.noreply.github.com>
2023-01-29 16:33:30 +0330

    update

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div className = 'row'>
             <div className="col-lo-5 col-md-7 mx-auto">
@@ -11,15 +13,25 @@ const Login = () => {
                         </h4>
                     </div>
                     <div className ="card-body border-bottom border-success">
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type = "text" className="form-control" id = "email" placeholder="Enter your Email"
-                            name = "email"/>
+                        <div className="form-group mt-2">
+                            <label className="mb-1" htmlFor="email">Email</label>
+                            <input 
+                                type = "text" 
+                                className="form-control" 
+                                id = "email" 
+                                placeholder="Enter your Email"
+                                name = "email"
+                            />
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type = "password" className="form-control" id = "password" placeholder="Enter your Password"
-                            name = "password"/>
+                        <div className="form-group mt-4">
+                            <label className="mb-1" htmlFor="password">Password</label>
+                            <input 
+                                type = "password" 
+                                className="form-control" 
+                                id = "password" 
+                                placeholder="Enter your Password"
+                                name = "password"
+                            />
                         </div>
                     </div>
                 </div>
