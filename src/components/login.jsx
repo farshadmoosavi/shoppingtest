5fb4efdfd72f356c64321cdb21f54345c0ceec10
farshadmoosavi <97333681+farshadmoosavi@users.noreply.github.com>
2023-01-29 15:45:21 +0330

    update

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,7 +5,23 @@ const Login = () => {
         <div className = 'row'>
             <div className="col-lo-5 col-md-7 mx-auto">
                 <div className="card border-success shodow-lg my-2">
-
+                    <div className="card-header border-bottom border-success">
+                        <h4 style = {{fontSize : "40px"}} className="text-success text-center">
+                            Login
+                        </h4>
+                    </div>
+                    <div className ="card-body border-bottom border-success">
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type = "text" className="form-control" id = "email" placeholder="Enter your Email"
+                            name = "email"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type = "password" className="form-control" id = "password" placeholder="Enter your Password"
+                            name = "password"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
