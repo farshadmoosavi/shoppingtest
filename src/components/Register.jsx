bd79e23ca39f9eb16815eb3d17121967f4328eab
farshadmoosavi <97333681+farshadmoosavi@users.noreply.github.com>
2023-02-15 13:09:50 +0330

    update

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 const Register = () => {
     const[state, setState] = useState({
-        email: '',
+        email: 'example@gmail.com',
         password: '',
         fullName: '',
         dateOfBirth: '',
@@ -15,8 +15,26 @@ const Register = () => {
         document.title = 'Register';
       },[])
     return (
-        <div>
-            Register
+        <div className="row">
+            <div className="col-lg-6 col-md-7 mx-auto">
+                <div className="card border-primary shadow my-3">
+                    <div className="card-header border-bottom border-primary">
+                        <h4 style={{fontSize :'40px'}} className='text-primary text-center'>Register</h4>
+                    </div>
+                    <div className="card-body border-primary">
+                        <div className="form-group form-row">
+                            <label className="col lg-4">Email</label>
+                                <input 
+                                    name = "email"
+                                    type="text" 
+                                    className="form-control" 
+                                    value={state.email} 
+                                    onChange={(e) => setState(...state, e.target.name = e.target.value)}
+                                />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
